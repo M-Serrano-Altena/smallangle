1c790d4b6bea5e76d1d00ac47179cee3ea621f2e
M-Serrano-Altena <Marc.Serrano.Altena@gmail.com>
2023-11-16 11:05:32 +0100

    added a command line interface with click

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,14 +3,20 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def geometric_func():
+    pass
 
+@geometric_func.command()
+@click.option('-n', '--number', default=10)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
-
+@geometric_func.command()
+@click.option('-n', '--number', default=10)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -19,4 +25,4 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    geometric_func()
