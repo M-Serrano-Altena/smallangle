39e1ad9173d964fb348f1af3bf2ce9f7cd04ea28
M-Serrano-Altena <Marc.Serrano.Altena@gmail.com>
2023-11-16 12:28:04 +0100

    added extra commands at the start

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,34 +1,55 @@
+# Analies Vlaar en Marc Serrano Altena
+# 16-11-2023
+# gives sin or tan values in n steps between 0 and 2 pi
+
 import click
 import numpy as np
-from numpy import pi
 import pandas as pd
+
 
 @click.group()
 def geometric_func():
+    """waits for either sin or tan command then runs the respective function"""
     pass
 
+
 @geometric_func.command()
-@click.option('-n', '--number', default=10)
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    type=int,
+    help="Amount of steps between 0 and 2 pi for the sin function",
+    show_default=True,
+)
 def sin(number):
     """Gives the sin value of n numbers between 0 and 2 pi
 
     Args:
         number (int): gives the amount of steps between 0 and 2 pi
-    """    
-    x = np.linspace(0, 2 * pi, number)
+    """
+    x = np.linspace(0, 2 * np.pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
+
 @geometric_func.command()
-@click.option('-n', '--number', default=10)
+@click.option(
+    "-n",
+    "--number",
+    type=int,
+    default=10,
+    help="Amount of steps between 0 and 2 pi for the tan function",
+    show_default=True,
+)
 def tan(number):
     """Gives the tan value of n numbers between 0 and 2 pi
 
     Args:
         number (int): gives the amount of steps between 0 and 2 pi
-    """    
-    x = np.linspace(0, 2 * pi, number)
+    """
+    x = np.linspace(0, 2 * np.pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
